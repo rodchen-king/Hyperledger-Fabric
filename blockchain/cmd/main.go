5d2cef204c0a00532181c5734a37576a5af1e285
rodchen <rodchen_king@outlook.com>
2018-10-18 23:07:17 +0800

    1. update main.go

--- blockchain/cmd/main.go
+++ blockchain/cmd/main.go
@@ -4,7 +4,10 @@ import "../core"
 
 func main() {
 	bc := core.CreateNewBlockChain()
-	bc.SetData(".")
-	bc.SetData("This is the second block.")
+	bc.SetData("A借B 100元，2018/09/01")
+	bc.SetData("A借C 200元，2018/09/05")
+	bc.SetData("A还B 50元， 2018/09/10")
+	bc.SetData("C借B 150元，2018/09/15")
+	bc.SetData("C还B 100元，2018/09/20")
 	bc.Print()
 }
